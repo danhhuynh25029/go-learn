032a4744e64de78c278d764e0179d54a97a60c3b
danhhnc <danhhnc@CSS-DanhHNC-M.local>
2023-11-29 15:18:57 +0700

    ex: rabbitmq

---
+++ rabbitmq/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	fmt.Println(q)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World"),
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Published Message to Queue")
+}
